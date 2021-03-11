391ed3ae444339f10fd41a1ff5168f0ce86317b3
Mathias Y. Zarate Diaz <mathias@Sakura.zidi.local>
2021-03-11 21:45:32 +0100

    decimal to binary converter added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("Typ hier uw decimale nummer in")
+
 decimal = int(input())
+
+binary = []
+ # function converts decimal to binary by dividing by 2 and checking for remainders. 
+while decimal != 0:
+    if decimal % 2 == 0:
+        decimal = decimal / 2
+        binary.insert(0,0)
+    else:
+        decimal = (decimal - 1) / 2
+        binary.insert(0,1)
