ed4b8a6f1487d2191f6ab80488f05dbe25c1828d
Mathias Y. Zarate Diaz <mathias@Sakura.zidi.local>
2021-03-11 22:44:26 +0100

    dec-bin function, crude menu for conversion selection

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-print("Typ hier uw decimale nummer in")
 
-decimal = int(input())
 
-binary = []
- # function converts decimal to binary by dividing by 2 and checking for remainders. 
-while decimal != 0:
-    if decimal % 2 == 0:
-        decimal = decimal / 2
-        binary.insert(0,0)
+def decToBin():
+    print("Typ hier uw decimale nummer in (integraal)")
+    decimalInput = int(input())
+
+    binary = []
+     # function converts decimal to binary by dividing by 2 and checking for remainders. 
+    while decimalInput != 0:
+        if decimalInput % 2 == 0:
+            decimalInput = decimalInput / 2
+            binary.insert(0,0)
+        else:
+            decimalInput = (decimalInput - 1) / 2
+            binary.insert(0,1)
+    print(binary)
+
+# Start script
+
+while True:
+    print("Binair naar decimaal of decimaal naar binair?")
+    print ("A=Dec-bin B=Bin-dec C=Programma verlaten")
+    # User input to choose converting method or to quit
+    
+    opt = input()
+    if opt == "A" or opt == "a":
+        print(" ")
+        decToBin()
+        print(" ")
+
+    elif opt == "B" or opt == "b":
+        print(" ")
+        print("Deze functie is nog niet beschikbaar")
+        print(" ")
+    
+    elif opt == "C" or opt == "c":
+        print(" ")
+        print("Dag!")
+        break
     else:
-        decimal = (decimal - 1) / 2
-        binary.insert(0,1)
+        print(" ")
+        print("Selecteer \"a\" of \"b\" a.u.b")
+        print(" ")
